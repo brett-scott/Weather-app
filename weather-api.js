05808a9f28bf8b75e2e997284678257bcb29f5d9
Brett Scott <br3ttscott@hotmail.com>
2018-12-15 22:01:52 +1000

    Removed debug console.log

--- weather-api.js
+++ weather-api.js
@@ -9,7 +9,6 @@ function getWeatherInfo(cityname) {
             url: weatherURL,
             json: true,
         }, function(error, response, body){
-            console.log(`Code: ${JSON.stringify(body.cod)} Type: ${JSON.stringify(typeof(body.cod))}`);
             if(error) return reject(error);
             if(body.cod === "404") return reject("City Not Found!");
             if(body.cod === 401) return reject("Invalid API Key");
